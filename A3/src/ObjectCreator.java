3aea30f50edadd0cbf3483dad9807c1954750669
msimiste <mikesimister@shaw.ca>
2016-11-02 22:48:33 -0600

    First commit, just getting started

---
+++ A3/src/ObjectCreator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class ObjectCreator {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+
+	}
+	
+	private void greeter(){
+		
+	}
+
+}
